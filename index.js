0005fcfade074e14e69351ccec5e566f0527c6e7
ascotttoney <ascotttoney@gmail.com>
2019-06-12 17:03:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
-// Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random) * 15
+
+const mod = 44 % 10
+
+const max = Math.max(5, 10, 15, 20)
